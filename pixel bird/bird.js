973a2c5172f24bbc4f274804ac20df0acef77e56
2689737232 <2689737232@qq.com>
2020-09-04 16:40:30 +0800

    添加了像素鸟文件夹

---
+++ pixel bird/bird.js
@@ -0,0 +1,10 @@
+class Bird {
+   constructor(width,height,bakImg) {
+      this.width = width;
+      this.height = height;
+      this.bakImg = bakImg;
+   }
+   fly(){
+      console.log("fly");  
+   }
+}
